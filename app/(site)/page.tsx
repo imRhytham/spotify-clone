cbdb82f0352db867f6b8470ab1bef74e70c957f8
imRhytham <rhythamp.404@gmail.com>
2023-09-14 16:33:10 +0530

    Added some reusable components

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -1,3 +1,40 @@
+import Header from "../components/Header";
+import ListItem from "../components/ListItem";
+
 export default function Home() {
-	return <div className="text-green-500">Hello World</div>;
+	return (
+		<div
+			className="
+			bg-neutral-900
+			rounded-lg
+			h-full
+			w-full
+			overflow-hidden
+			overflow-y-auto
+	"
+		>
+			<Header>
+				<div className="mb-2">
+					<h1 className="text-3xl text-white font-semibold">Welcome Back</h1>
+					<div
+						className="
+						grid
+						grid-cols-1
+						sm:grid-cols-2
+						xl:grid-cols-3
+						2xl:grid-cols-4
+						gap-3
+						mt-4
+					"
+					>
+						<ListItem
+							name="Liked Songs"
+							image="/images/liked.png"
+							href="liked"
+						/>
+					</div>
+				</div>
+			</Header>
+		</div>
+	);
 }
