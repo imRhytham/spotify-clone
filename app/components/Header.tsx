719b7e2bd6822c963e08e9ac4fbcc820a5c0bef7
imRhytham <rhythamp.404@gmail.com>
2024-01-04 18:44:11 +0530

    added auth functionality with supabase

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -5,6 +5,11 @@ import { RxCaretLeft, RxCaretRight } from "react-icons/rx";
 import { HiHome } from "react-icons/hi";
 import { BiSearch } from "react-icons/bi";
 import Button from "./Button";
+import useAuthModal from "@/hooks/useAuthModal";
+import { useSupabaseClient } from "@supabase/auth-helpers-react";
+import { useUser } from "@/hooks/useUser";
+import { FaUserAlt } from "react-icons/fa";
+import toast from "react-hot-toast";
 
 interface HeaderProps {
 	children: React.ReactNode;
@@ -13,8 +18,18 @@ interface HeaderProps {
 
 const Header: React.FC<HeaderProps> = ({ children, className }) => {
 	const router = useRouter();
+	const { onOpen } = useAuthModal();
+	const supabaseClient = useSupabaseClient();
+	const { user } = useUser();
 
-	const handleLogout = () => {};
+	const handleLogout = async () => {
+		const { error } = await supabaseClient.auth.signOut();
+		//TODO: Reset any playing song
+		router.refresh();
+		if (error) {
+			toast.error(error.message);
+		} else toast.success("Logged out!");
+	};
 
 	return (
 		<div
@@ -119,14 +134,37 @@ const Header: React.FC<HeaderProps> = ({ children, className }) => {
             "
 				>
 					<>
-						<div>
-							<Button className="bg-transparent text-neutral-300 font-medium">
-								Sign up
-							</Button>
-						</div>
-						<div>
-							<Button className="bg-white px-6 py-2">Login</Button>
-						</div>
+						{user ? (
+							<>
+								<div className="flex gap-x-4 items-center">
+									<Button className="bg-white px-6 py-2" onClick={handleLogout}>
+										Logout
+									</Button>
+									<Button
+										className="bg-white"
+										onClick={() => router.push("/account")}
+									>
+										<FaUserAlt />
+									</Button>
+								</div>
+							</>
+						) : (
+							<>
+								<div>
+									<Button
+										onClick={onOpen}
+										className="bg-transparent text-neutral-300 font-medium"
+									>
+										Sign up
+									</Button>
+								</div>
+								<div>
+									<Button onClick={onOpen} className="bg-white px-6 py-2">
+										Login
+									</Button>
+								</div>
+							</>
+						)}
 					</>
 				</div>
 			</div>
--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,9 @@ import type { Metadata } from "next";
 import { Figtree } from "next/font/google";
 import Sidebar from "./components/Sidebar";
 import SupabaseProvider from "@/providers/SupabaseProvider";
-import UserProvider from "@/providers/userProvider";
+import UserProvider from "@/providers/UserProvider";
+import ModalProvider from "@/providers/ModalProvider";
+import ToastProvider from "@/providers/ToasterProvider";
 
 const font = Figtree({ subsets: ["latin"] });
 
@@ -20,8 +22,10 @@ export default function RootLayout({
 	return (
 		<html lang="en">
 			<body className={font.className}>
+				<ToastProvider />
 				<SupabaseProvider>
 					<UserProvider>
+						<ModalProvider />
 						<Sidebar>{children}</Sidebar>
 					</UserProvider>
 				</SupabaseProvider>
